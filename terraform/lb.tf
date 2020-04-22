6c6c6bf594476d92ac06d0e1f3c9e213ab71de2a
Sinelnikov Timofei <tjoker@yandex.ru>
2020-04-22 16:15:03 +1000

    HW Terraform-1 V4 (fix lb)

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -1,50 +1,23 @@
-#Ресурс для создания белого адреса
-#resource "google_compute_global_address" "default" {
-#  name = "global-app-ip"
-#}
-#Ресурс создания группы инстансов
-resource "google_compute_instance_group" "puma-app" {
-  name      = "puma-app-group"
-  instances = [google_compute_instance.app[0].self_link, google_compute_instance.app[1].self_link]
-  zone      = var.zone
-  named_port {
-    name = "http"
-    port = "9292"
-  }
-}
-
 #Проверка состояния
 resource "google_compute_http_health_check" "default" {
   name               = "health-check"
   request_path       = "/"
-  check_interval_sec = 1
-  timeout_sec        = 1
+  check_interval_sec = 10
+  timeout_sec        = 5
+  port = "9292"
+
 }
 
-#Ресурс бэкенд
-resource "google_compute_backend_service" "puma-app-backend" {
-  name          = "puma-app-backend"
+#Ресурс пул VM
+resource "google_compute_target_pool" "puma-app-pool" {
+  name          = "puma-app"
+  instances = google_compute_instance.app[*].self_link
   health_checks = [google_compute_http_health_check.default.self_link]
-  #backend {group = google_compute_instance_group.puma-app-group}
-}
-# создаем балансировщик нагрузки
-resource "google_compute_url_map" "urlmap" {
-  name            = "urlmap"
-  default_service = google_compute_backend_service.puma-app-backend.self_link
 }
 
-# https://cloud.google.com/load-balancing/docs/target-proxies
-# создаем целевой прокси сервер
-resource "google_compute_target_http_proxy" "default" {
-  name    = "puma-proxy"
-  url_map = google_compute_url_map.urlmap.self_link
-
-
-}
-
-# добавляем правила обработки трафика
-resource "google_compute_global_forwarding_rule" "default" {
-  name       = "global-rule"
-  target     = google_compute_target_http_proxy.default.self_link
-  port_range = "80"
+# добавляем правило перенаправления трафика
+resource "google_compute_forwarding_rule" "default" {
+  name       = "lb"
+  target     = google_compute_target_pool.puma-app-pool.self_link
+  port_range = "9292"
 }
